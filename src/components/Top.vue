f706f03de97bf80c957858d5fc41d6c4aeb1bc9d
osawa.tomohisa <kebin090@icloud.com>
2019-08-14 00:37:04 +0900

    pagetop追加

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -5,6 +5,7 @@
     <works></works>
     <about></about>
     <toolskill></toolskill>
+    <go-top></go-top>
   </main>
 </template>
 
@@ -14,6 +15,7 @@
   import works from "@/components/parts/top/Works";
   import about from "@/components/parts/top/About";
   import toolskill from "@/components/parts/top/ToolSkill";
+  import GoTop from '@inotom/vue-go-top';
 
   export default {
     name: "top",
@@ -27,12 +29,19 @@
       works: works,
       about: about,
       toolskill: toolskill,
+      GoTop
     }
 
   };
 </script>
 
 <style lang="scss" scoped>
+  .vue-go-top{
+    background-color: #1390ff!important;
+    outline: none;
+  }
+
+
   @media (max-width: 480px) {
     .main {
       background: url(/static/img/main.ad3ebec.jpg) no-repeat;
